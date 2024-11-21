a1ea852c28b1e3c9256d850093768193be81c01f
Cyril GERMAINE <c.germaine@exosens.com>
2024-11-21 18:27:43 +0100

    Support of Dione for Ubuntu

--- sources/rootfs/usr/bin/eg_media_configure_rp1_cfe.sh
+++ sources/rootfs/usr/bin/eg_media_configure_rp1_cfe.sh
@@ -4,7 +4,7 @@ media=$(v4l2-ctl --list-devices | grep rp1-cfe -A 20 | grep -m1 /dev/media)
 if [[ x$media != x ]]
 then
    echo media = $media
-   fmt=$(media-ctl -p -d $media | grep -E "entity.*dioneir" -A 10 | grep -m1 fmt)
+   fmt=$(media-ctl -p -d $media | grep -E "entity.*dioneir" -A 10 | grep -m1 fmt | sed 's/stream:*. //')
    echo fmt = $fmt
 
    media-ctl -d $media -l ''\''csi2'\'':4 -> '\''rp1-cfe-csi2_ch0'\'':0 [1]'
